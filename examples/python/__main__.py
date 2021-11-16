22c974b0dc9b149bfc986c3ea71b56ddf73cbf45
Anirudh <anirudhy@unifytech.com>
2021-11-16 15:55:10 +0530

    python example

---
+++ examples/python/__main__.py
@@ -0,0 +1,9 @@
+"""An Azure RM Python Pulumi program"""
+# python -m pip install -e location\to\pulumi-azure-quickstart-compute\sdk\python
+
+
+from pulumi_azure_quickstart_compute import virtualmachine as vm
+
+# Create an Azure virtual machine
+
+vm1 = vm.Virtualmachine("vm1",vm.VirtualmachineArgs(name="workload1",admin_username="admin123",admin_password="Unify123$",location="westus",vm_size="Standard_D2s_v3"))
